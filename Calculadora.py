43928db263cd0f5f3d6a65ceba0fc63b539a470f
Høster <gabrielfreirelm@gmail.com>
2024-07-12 08:37:39 -0300

    Correçãozinha

--- Calculadora.py
+++ Calculadora.py
@@ -1,9 +1,9 @@
-opr = str(input('''Escolha a operação que deseja fazer dentre as opções abaixo))
+opr = str(input(""""Escolha a operação que deseja fazer dentre as opções abaixo))
             + = soma
             * = multiplicação
             / = divisão
             - = subtração
-            Digite o número da operação que deseja fazer: '''))
+            Digite a operação que deseja fazer: """))
 num1 = int(input("Digite o primeiro número: "))
 num2 = int(input("Digite o segundo número: "))
 if opr == "+": 
